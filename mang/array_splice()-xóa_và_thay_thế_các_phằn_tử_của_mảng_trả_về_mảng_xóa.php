338e1425c316229c8a0562919e9e747428d6fd37
nguyenvanhao1209 <88066192+nguyenvanhao1209@users.noreply.github.com>
2022-01-07 21:03:12 +0700

    Create array_splice()-xóa_và_thay_thế_các_phằn_tử_của_mảng_trả_về_mảng_xóa.php

---
+++ mang/array_splice()-xóa_và_thay_thế_các_phằn_tử_của_mảng_trả_về_mảng_xóa.php
@@ -0,0 +1,11 @@
+<?php
+echo '<pre>';
+
+$a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
+$a2=array("a"=>"purple","b"=>"orange");
+print_r(array_splice($a1,0,2,$a2));
+
+print_r($a1);
+
+echo '</pre>';
+?>
