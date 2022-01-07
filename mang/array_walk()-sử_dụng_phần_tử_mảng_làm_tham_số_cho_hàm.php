1f6c99b9b7785e67bf000003d7ce061bdc051287
nguyenvanhao1209 <88066192+nguyenvanhao1209@users.noreply.github.com>
2022-01-07 21:46:42 +0700

    Create array_walk()-sử_dụng_phần_tử_mảng_làm_tham_số_cho_hàm.php

---
+++ mang/array_walk()-sử_dụng_phần_tử_mảng_làm_tham_số_cho_hàm.php
@@ -0,0 +1,12 @@
+<?php
+echo '<pre>';
+
+function myfunction($value,$key)
+{
+echo "The key $key has the value $value<br>";
+}
+$a=array("a"=>"red","b"=>"green","c"=>"blue");
+array_walk($a,"myfunction");
+
+echo '</pre>';
+?>
