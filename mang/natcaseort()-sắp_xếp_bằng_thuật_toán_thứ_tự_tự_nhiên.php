360ee6bc4ff5966f13160b00d390d8276ec26034
nguyenvanhao1209 <88066192+nguyenvanhao1209@users.noreply.github.com>
2022-01-08 07:14:44 +0700

    Create natcaseort()-sắp_xếp_bằng_thuật_toán_thứ_tự_tự_nhiên.php

---
+++ mang/natcaseort()-sắp_xếp_bằng_thuật_toán_thứ_tự_tự_nhiên.php
@@ -0,0 +1,17 @@
+<?php
+echo '<pre>';
+
+$temp_files = array("temp15.txt","Temp10.txt",
+"temp1.txt","Temp22.txt","temp2.txt");
+
+natsort($temp_files);
+echo "Natural order: ";
+print_r($temp_files);
+echo "<br />";
+
+natcasesort($temp_files);
+echo "Natural order case insensitve: ";
+print_r($temp_files);
+
+echo '</pre>';
+?>
